a494fd8f9d880a87b9f122b8db5de4f9348d1aaf
kown <skillove@gmail.com>
2016-07-25 23:17:13 +0900

    working

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,10 @@
 <?php
 
-// Require the extension's composer autoload file. This will enable all of our
-// classes in the src directory to be autoloaded.
-require __DIR__.'/vendor/autoload.php';
+use Flarum\Event\ConfigureLocales;
+use Illuminate\Contracts\Events\Dispatcher;
 
-// Return the name of our Extension class. Flarum will register it as a service
-// provider, allowing it to register bindings and execute code when the
-// application boots.
-return 'iycode\Extension';
+return function (Dispatcher $events) {
+    $events->listen(ConfigureLocales::class, function (ConfigureLocales $event) {
+        $event->loadLanguagePackFrom(__DIR__);
+    });
+};
